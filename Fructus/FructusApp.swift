1dc136d09fd079c65f3a6671a0d89ff6b7f082d5
HERYORDEJY <heryordejy.dev@gmail.com>
2023-08-07 14:07:56 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by  OYEBODE YUSUF AYODEJI on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
